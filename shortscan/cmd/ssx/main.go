7f9eec0697334078f53e83d4887c070e4fceb5e3
Serdar <serdar@example.com>
2026-02-24 16:32:07 +0300

    Enhancement: Rename tool from shortscan to ssx
    - Renamed command directories to ssx
    - Updated version strings and help text
    - Updated README documentation for ssx command usage

---
+++ shortscan/cmd/ssx/main.go
@@ -0,0 +1,16 @@
+// ----------------------------------------------------------
+// SSX
+// An IIS short filename enumeration tool written by bitquark
+// ----------------------------------------------------------
+// Docs and code: https://github.com/bitquark/shortscan
+// ----------------------------------------------------------
+
+package main
+
+import (
+	"github.com/bitquark/shortscan/pkg/shortscan"
+)
+
+func main() {
+	shortscan.Run()
+}
